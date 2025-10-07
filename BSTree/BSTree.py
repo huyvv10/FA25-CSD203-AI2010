a27f57355420ee8b917167733d798a5f0816432f
Vu Van Huy <huyvv10@fpt.edu.vn>
2025-10-07 12:16:00 +0700

    Commit

--- BSTree/BSTree.py
+++ BSTree/BSTree.py
@@ -1,3 +1,4 @@
+from Queue import Queue
 from Node import Node
 class BSTree:
     def __init__(self, root=None):
@@ -59,3 +60,173 @@ class BSTree:
         if root.right is not None:
             self.postOrder(root.right)	#Vissit right
         print(root.info, end=' ')		#Visit root
+
+    def visit(self, n):
+        if n is None:
+            return
+        print(n.info,end=' ')
+    
+    def breadthFirstTraversal(self):
+        if self.isEmpty():
+            return
+        myQ=Queue()
+        myQ.enqueue(self.root)
+        while not myQ.isEmpty():
+            p=myQ.dequeue()
+            self.visit(p)
+            if p.left is not None:
+                myQ.enqueue(p.left)
+            if p.right is not None:
+                myQ.enqueue(p.right)
+    
+    def countTreeNodes(self):
+        if self.isEmpty():
+            return 0
+        count=0
+        myQ=Queue()
+        myQ.enqueue(self.root)
+        while not myQ.isEmpty():
+            p=myQ.dequeue()
+            count+=1
+            if p.left is not None:
+                myQ.enqueue(p.left)
+            if p.right is not None:
+                myQ.enqueue(p.right)        
+        return count
+
+    def countTreeNodesDFS(self, root):
+        if self.isEmpty():
+            return 0
+        c=l=r=0
+        if root.left is not None:		#Visit left
+            l=self.countTreeNodesDFS(root.left)
+        c+=1		#Visit root
+        if root.right is not None:
+            r=self.countTreeNodesDFS(root.right)	#Vissit right
+        return c+l+r            
+        
+    def countNodesHasTwoChildren(self):
+        if self.isEmpty():
+            return 0
+        count=0
+        myQ=Queue()
+        myQ.enqueue(self.root)
+        while not myQ.isEmpty():
+            p=myQ.dequeue()
+            if p.left is not None and p.right is not None:	#Check condition
+                count+=1
+            if p.left is not None:
+                myQ.enqueue(p.left)
+            if p.right is not None:
+                myQ.enqueue(p.right)        
+        return count
+    
+    def countNodesHasOnlyLeftChild(self):
+        if self.isEmpty():
+            return 0
+        count=0
+        myQ=Queue()
+        myQ.enqueue(self.root)
+        while not myQ.isEmpty():
+            p=myQ.dequeue()
+            if p.left is not None and p.right is None:	#Check condition
+                count+=1
+            if p.left is not None:
+                myQ.enqueue(p.left)
+            if p.right is not None:
+                myQ.enqueue(p.right)        
+        return count
+    
+    def countNodesHasOnlyRightChild(self):
+        if self.isEmpty():
+            return 0
+        count=0
+        myQ=Queue()
+        myQ.enqueue(self.root)
+        while not myQ.isEmpty():
+            p=myQ.dequeue()
+            if p.left is None and p.right is not None:	#Check condition
+                count+=1
+            if p.left is not None:
+                myQ.enqueue(p.left)
+            if p.right is not None:
+                myQ.enqueue(p.right)        
+        return count
+    
+    #Node has atleast one child
+    def countInternalNodes(self):
+        if self.isEmpty():
+            return 0
+        count=0
+        myQ=Queue()
+        myQ.enqueue(self.root)
+        while not myQ.isEmpty():
+            p=myQ.dequeue()
+            if p.left is not None or p.right is not None:	#Check condition
+                count+=1
+            if p.left is not None:
+                myQ.enqueue(p.left)
+            if p.right is not None:
+                myQ.enqueue(p.right)        
+        return count
+    
+    #Count number of leaf nodes
+    def countExternalNode(self):
+        if self.isEmpty():
+            return 0
+        count=0
+        myQ=Queue()
+        myQ.enqueue(self.root)
+        while not myQ.isEmpty():
+            p=myQ.dequeue()
+            if p.left is None and p.right is None:	#Check condition
+                count+=1
+            if p.left is not None:
+                myQ.enqueue(p.left)
+            if p.right is not None:
+                myQ.enqueue(p.right)        
+        return count
+
+    def countNodesAboveThreshold(self, x):
+        if self.isEmpty():
+            return 0
+        count=0
+        myQ=Queue()
+        myQ.enqueue(self.root)
+        while not myQ.isEmpty():
+            p=myQ.dequeue()
+            if p.info>x:	#Check condition
+                count+=1
+            if p.left is not None:
+                myQ.enqueue(p.left)
+            if p.right is not None:
+                myQ.enqueue(p.right)        
+        return count
+    
+    #Cound number of node with value in range [x,y]
+    def countNodesAboveThreshold(self, x, y):
+        if self.isEmpty():
+            return 0
+        if x>y:
+            t=x; x=y; y=t
+        count=0
+        myQ=Queue()
+        myQ.enqueue(self.root)
+        while not myQ.isEmpty():
+            p=myQ.dequeue()
+            if p.info>=x and p.info<=y:	#Check condition
+                count+=1
+            if p.left is not None:
+                myQ.enqueue(p.left)
+            if p.right is not None:
+                myQ.enqueue(p.right)        
+        return count
+    
+    def deleteByCopyingLeftSide(self, x):
+        pass
+    
+    def deleteByCopyingRightSide(self, x):
+        pass
+    
+    def deleteByMerging(self, x):
+        pass
---
+++ BSTree/Queue.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self, info, next=None):
+        self.info=info
+        self.next=next
+        pass
+
+class Queue:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+    
+    def isEmpty(self):
+        return self.head==None
+    
+    def enqueue(self, x):
+        newNode=Node(x)
+        if self.isEmpty():
+            self.head=self.tail=newNode
+        else:
+            self.tail.next=newNode
+            self.tail=newNode
+    
+    def dequeue(self):
+        if self.isEmpty():
+            print("Error. Queue is empty now.")
+            return
+        x=self.head.info
+        if self.head.next is None:	#Queue has only 1 node
+            self.head=self.tail=None
+            return x
+        self.head=self.head.next	#Move head to the next node
+        return x
+    
+    def first(self):
+        if self.isEmpty():
+            print("Queue is empty now.")
+            return
+        return self.head.info
+    
+    def size(self):
+        count=0
+        cur=self.head
+        while cur:
+            count+=1
+            cur=cur.next
+        return count        
